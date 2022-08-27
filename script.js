9f764833e9beb4e29b79892c3f8d9865d366df1e
Oyebola Daramola <oyebolang@gmail.com>
2022-08-27 21:42:48 +0100

    latest changes to chibestalu

---
+++ script.js
@@ -0,0 +1,4 @@
+let gridImg = document.getElementById("grid-img")
+gridImg.addEventListener('click', () =>{
+    gridImg.style.cssText = " z-index: 34; width: 600px; height: 400px; margin: auto;"
+})
