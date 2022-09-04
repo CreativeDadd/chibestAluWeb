9515d64b98ad8558de6b65f9bac048a57e83088c
Oyebola Daramola <oyebolang@gmail.com>
2022-09-04 17:02:44 +0100

    modified version

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-let gridImg = document.getElementById("grid-img")
-gridImg.addEventListener('click', () =>{
-    gridImg.style.cssText = " z-index: 34; width: 600px; height: 400px; margin: auto;"
+let menuIcon = document.getElementById("menu-icon");
+let navUl = document.getElementById('nav-ul');
+let body = document.querySelector("body");
+
+
+menuIcon.addEventListener('click', function(){
+    navUl.classList.toggle('active')
+})
+
+window.addEventListener('click', function(e) {
+    if(e.target == body) {
+        navUl.style.display= 'none';
+    }
 })
