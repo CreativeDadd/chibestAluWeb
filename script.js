1d773c34213868f4ae0919e7742799fc66f4b09e
Oyebola Daramola <oyebolang@gmail.com>
2022-09-26 07:36:30 +0100

    Chibest mobile update

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let menuIcon = document.getElementById("menu-icon");
 let navUl = document.getElementById('nav-ul');
 let body = document.querySelector("body");
-
+let floatArrowRight = document.querySelector(".fa-arrow-right");
+let floatEl = document.querySelector(".cta-side");
+let faArrowLeft = document.querySelector(".fa-arrow-left");
 
 menuIcon.addEventListener('click', function(){
     navUl.classList.toggle('active')
@@ -13,6 +15,11 @@ window.addEventListener('click', function(e) {
     }
 })
 
+floatArrowRight.addEventListener('click', ()=>{
+    // console.log("success");
+    floatEl.style.position = ''
+    faArrowLeft.style.display = 'block
+})
 
 
 
