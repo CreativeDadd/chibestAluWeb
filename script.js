ecf0ce3f52da8d3bba2ccf749f3a2d3555f68477
Oyebola Daramola <oyebolang@gmail.com>
2022-10-10 00:17:53 +0100

    Latest Changes to 'Our Products Sections'

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let menuIcon = document.getElementById("menu-icon");
 let navUl = document.getElementById('nav-ul');
+let menuX = document.getElementById('menu_x')
 let body = document.querySelector("body");
 let floatArrowRight = document.querySelector(".fa-arrow-right");
 let floatEl = document.querySelector(".cta-side");
@@ -7,13 +8,25 @@ let faArrowLeft = document.querySelector(".fa-arrow-left");
 let floatCaontact = document.querySelector(".float-contact");
 
 menuIcon.addEventListener('click', function(){
-    navUl.classList.toggle('active')
+    navUl.classList.toggle('active');
+    menuIcon.style.display = 'none'
+    menuX.style.display = 'block'
+
+
 })
 
-window.addEventListener('click', function(e) {
-    if(e.target === body) {
+navUl.addEventListener('click', function() {
         navUl.classList.remove('active');
-    }
+        menuIcon.style.display = 'flex';
+    menuX.style.display = 'none';
 })
 
+menuX.addEventListener('click', ()=>{
+    navUl.classList.remove('active');
+    menuIcon.style.display = 'flex'
+    menuX.style.display = 'none'
 
+
+
+    
+})
